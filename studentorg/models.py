5e29d3787449e6ec25db7b1101e361b4427fdf50
Teodore James M. Ponce de Leon <teodorejames@gmail.com>
2025-04-01 22:28:34 +0800

    fixed model

--- studentorg/models.py
+++ studentorg/models.py
@@ -21,7 +21,7 @@ class Program(BaseModel):
         return self.prog_name
 class Organization(BaseModel):
     name = models.CharField(max_length=150)
-    College = models.ForeignKey(
+    college = models.ForeignKey(
         College, null=True, blank=True, on_delete=models.CASCADE)
     description = models. CharField(max_length=500)
 
